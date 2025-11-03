d1749699ba4fa595ecb55d8bfa961793da96046f
sea2horses <thefuzegamer06@gmail.com>
2025-11-03 17:52:24 -0600

    Agregar entidad direccion (Lección 2)

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Direccion.java
@@ -0,0 +1,24 @@
+package ni.edu.uam.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Setter
+public class Direccion {
+    @Column (length = 30)
+    String viaPublica;
+
+    @Column(length = 5)
+    int codigoPostal;
+
+    @Column(length = 20)
+    String municipio;
+
+    @Column(length = 30)
+    String provincia;
+}
