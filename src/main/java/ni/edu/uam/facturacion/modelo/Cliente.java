5db5ca4ad9aede4d52adcfdcc76a5c71b91cc3e2
sea2horses <thefuzegamer06@gmail.com>
2025-11-03 17:48:24 -0600

    Linkear Productos y Categorias (Lección 2)

--- src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
@@ -1,4 +1,21 @@
 package ni.edu.uam.facturacion.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
 public class Cliente {
+    @Id
+    @Column(length=6)
+    int numero;
+
+    @Column(length=50)
+    @Required
+    String nombre;
 }
--- src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
@@ -1,4 +1,27 @@
 package ni.edu.uam.facturacion.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.DescriptionsList;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
 public class Producto {
+    @Id
+    @Column(length=9)
+    int numero;
+
+    @Column(length=50)
+    @Required
+    String descripcion;
+
+    @ManyToOne(
+            fetch= FetchType.LAZY,
+            optional = true
+    )
+    @DescriptionsList
+    Categoria categoria;
 }
