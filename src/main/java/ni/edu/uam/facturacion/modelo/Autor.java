5ac22e82f19511148d7755f927e87484a799f254
sea2horses <thefuzegamer06@gmail.com>
2025-11-03 18:28:52 -0600

    Agregar Entidad Autor (Lección 5)

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Autor.java
@@ -0,0 +1,23 @@
+package ni.edu.uam.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Autor {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=50)
+    String nombre;
+}
