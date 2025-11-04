b6bd5c5792576f81e32e15740e4541496187c987
sea2horses <thefuzegamer06@gmail.com>
2025-11-03 18:13:48 -0600

    Añadir Entidad Detalle (Lección 3)

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Detalle.java
@@ -0,0 +1,17 @@
+package ni.edu.uam.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+@Getter @Setter
+public class Detalle {
+    int cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    Producto producto;
+}
